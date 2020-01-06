c8680a16165ca6645b4e546f55135f77678e510a
Osvaldo Gioia <osvaldogioia@hotmail.com>
2020-01-06 18:33:29 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+# Los Simpsons Frases API
+
+A simple API de Los Simpsons!
+
+## Production host
+
+[https://los-simpsons-quotes.herokuapp.com](https://los-simpsons-quotes.herokuapp.com)
+
+## API
+
+### `GET /v1/quotes`
+
+Get a random quote in this format:
+
+> [https://los-simpsons-quotes.herokuapp.com/v1/quotes](https://los-simpsons-quotes.herokuapp.com/v1/quotes)
+
+
+### `GET /v1/quotes/{number}`
+
+Returns an array with `{number}` quotes e.g. `GET /v1/quotes/5`.
+
+> [https://los-simpsons-quotes.herokuapp.com/v1/quotes/5](https://los-simpsons-quotes.herokuapp.com/v1/quotes/5)
+
+
+## Creditos
+
+Inspirado en [Breking Bad Quotes](https://github.com/shevabam/breaking-bad-quotes).
---
+++ quotes.js
@@ -0,0 +1,216 @@
+'use strict';
+
+module.exports = [
+    {
+        "quote": "A la grande le puse Cuca.",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "¡¿Alguien quiere pensar en los niños?!",
+        "author": "Helena Alegría"
+    },
+    {
+        "quote": "Tiene todo el dinero del mundo, pero hay algo que no puede comprar… Un dinosaurio.",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "¡No vives de ensalada!",
+        "author": "Homero, Bart & Marge Simpson"
+    },
+    {
+        "quote": "El alcohol: la causa y la solución de todos los problemas de la vida.",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "Sin televisión y sin cerveza Homero pierde la cabeza.",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "¿Te acuerdas de Alf? ¡Volvió! ¡En forma de fichas!",
+        "author": "Milhouse Van Houten"
+    },
+    {
+        "quote": "Vas a morir, Moe. Wiiiiiii",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "¿Dónde está mi hamburguesa?",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "Es como si no llevara nada puesto ...nada puesto ...nada puesto.",
+        "author": "Ned Flanders"
+    },
+    {
+        "quote": "¡Momento! ¿La maestra de Bart se apellida Krabappel? ¡Yo le decia Clavados! ¿Por que nadie me lo dijo?, Debo haber parecido un idiota!",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "¿Qué te pasó, viejo? Antes eras chévere.",
+        "author": "Bart Simpson"
+    },
+    {
+        "quote": "No lloren por mí. Ya estoy muerto.",
+        "author": "Barney Gómez"
+    },
+    {
+        "quote": "Es mi primer día.",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "Sin la presencia de un hombre en la casa puedes volverte afeminado en un segundo. Ay, esta grasa no se quita.",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "Les traigo paz.",
+        "author": "Montgomery Burns"
+    },
+    {
+        "quote": "Niños: Hicieron su esfuerzo y fracasaron miserablemente. La lección es: nunca se esfuercen.",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "En un momento así, solo se puede reír.",
+        "author": "Marge Simpson"
+    },
+    {
+        "quote": "Siempre dudé de la existencia de Dios. Ahora sé que existe, soy yo.",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "Me gusta la cerveza fría, la tele fuerte y los homosexuales locas, locas si",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "Plan dental... Lisa necesita frenos.",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "Ahora vamos a solucionar nuestros problemas con un gran platón de helado de vainilla.",
+        "author": "Marge Simpson"
+    },
+    {
+        "quote": "Mi Homero no es comunista. Podrá ser mentiroso, puerco, idiota, comunista, pero nunca una estrella de porno.",
+        "author": "Abe Simpson"
+    },
+    {
+        "quote": "El matrimonio es igual que una naranja. Primero está la piel, luego la dulce, dulce pulpa.",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "Hable más fuerte que tengo una toalla.",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "Lisa, los vampiros son seres inventados, como los duendes, los gremlins y los esquimales.",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "Normalmente no rezo, pero si estás ahí­, por favor, ¡sálvame Superman!",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "Los viejos no necesitan amor, tienen que ser aislados y estudiados para ver qué nutrientes se les pueden sacar para nuestro beneficio personal.",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "Trabajo mucho y quiero a mis hijos, ¿por qué voy a pasar la mitad del domingo oyendo que me voy a ir al infierno?",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "No vayas a decir que es un bar ¿Pero qué más abre a esa hora? Es una tienda de pornografía, estaba comprando pornografía. Jejejeje, que buenas ideas tengo.",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "Yuju, ya soy universitario. ya no necesito el diploma de la escuela secundaria. Soy intelectual, muy inteligente. Soy intelectual, muy inteligente, hay que bonito soy.",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "Bart no quiero asustarte pero tal vez el Coco, el Coco esta en la casa.",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "Si yo me muriera, reencarnarí­a en mariposa, nadie sospecharí­a de una mariposa.",
+        "author": "Bart Simpson"
+    },
+    {
+        "quote": "Oye, Otto, ¡tengo un examen hoy y no estoy listo! ¿Podrías estrellar el autobús o algo?",
+        "author": "Bart Simpson"
+    },
+    {
+        "quote": "Lisa, tu tienes la inteligencia para llegar hasta donde quieras. Y cuando llegues, yo estaré contigo para pedirte prestado.",
+        "author": "Bart Simpson"
+    },
+    {
+        "quote": "Vive deprisa, muere joven y deja un cadáver obeso.",
+        "author": "Bart Simpson"
+    },
+    {
+        "quote": "No puedes esconderte de mí, paso 23 horas diarias aqui.",
+        "author": "Marge Simpson"
+    },
+    {
+        "quote": "Homero, me dijiste que no ibas a usar al muñeco para fingir tu muerte.",
+        "author": "Marge Simpson"
+    },
+    {
+        "quote": "Tu padre es muy sensible a veces ¿Recuerdas cuando me reí de su gorra de Sherlock Holmes? Lloró una semana y cerró su agencia de detectives...",
+        "author": "Marge Simpson"
+    },
+    {
+        "quote": "Su vida fue tuvo éxitos desenfrenados hasta que se dio cuenta que era un Simpson.",
+        "author": "Lisa Simpson"
+    },
+    {
+        "quote": "¡Oh, no! ¡Elecciones! ¿Es uno de esos días en que cierran las tabernas, no es cierto?",
+        "author": "Barney Gómez"
+    },
+    {
+        "quote": "¡La academia me odia, son una banda de dinosaurios, saben menos de television que de los pelos que tienen en las ... eh, hooooolaaaa!",
+        "author": "Krusty"
+    },
+    {
+        "quote": "¡Ahhhh, Marge, no te voy a mentir!… ¡Adios!",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "¿Un Gym?¿Qué es un gym? ¡Ah, un gym!",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "En mi familia somos 5 personas: Marge, Bart, la niña Bart, la que nunca habla y el tipo gordo, cómo lo desprecio",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "¡Jaque mate doña cama elástica!",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "¡Que elegancia la de Francia!",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "Y todo se lo debo a este simpático felino.",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote": "¡No se rían de mi, pueden tener un hijo igual",
+        "author": "Homero Simpson"
+    },
+    {
+        "quote":"Lisa lloró, yo lloré y Maggie rió. Todo fue una confusión.",
+        "author":"Homero Simpson"
+    },
+    {
+        "quote":"Marge, niños, les prometo que todo va a salir bien. Vayan arriba, empaquen sus maletas. Vamos a comenzar una nueva vida… Bajo el mar.",
+        "author":"Homero Simpson"
+    },
+    {
+        "quote":"¿Ah, si? ¿Y si era tan listo por qué se murió?",
+        "author":"Homero Simpson"
+    },
+    {
+        "quote":"Disimula y ve despacio hacia el pastel.",
+        "author":"Homero Simpson"
+    }
+];
